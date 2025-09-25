38832eca12a52ffbd9c0c5ce8647c20ca107b661
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-09-25 11:00:36 +0200

    Initial commit with base app structure

---
+++ Ext/app/(tabs)/workouts.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Workouts = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default Workouts
---
+++ README.md
@@ -0,0 +1,1 @@
+Ext is a workout tracking app built on React Native + Expo and Zustand for state. 
