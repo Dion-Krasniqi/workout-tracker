b1006cb8338a8326bb7e4ed60bd1f4034654911b
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-09-30 16:31:30 +0200

    Dropdown menu done just have to mess around a bit with styling, epecially with some shadow appearing at the bottom when pressed. Also added some template options so I need to extract from muscle_group database and then also send back corresponding id

--- Ext/Components/dropDown.tsx
+++ Ext/Components/dropDown.tsx
@@ -1,6 +1,79 @@
-import { View, Text } from 'react-native'
-import React, {ReactNode} from 'react'
+import { View, Text, TouchableOpacity, TouchableWithoutFeedback, Modal, StyleSheet } from 'react-native'
+import React, {ReactNode, useEffect, useRef, useState} from 'react'
 
-export const menuTrigger = ({children}:{children: ReactNode}) => {
+export const MenuTrigger = ({children}:{children: ReactNode}) => {
   return <>{children}</>
 }
+
+
+export const MenuOption = ({onSelect, children}:{onSelect: ()=> void; children: ReactNode}) =>{
+  return (
+    <TouchableOpacity onPress={onSelect} className='p-1 '>
+      {children}
+    </TouchableOpacity>
+  )
+}
+
+interface DropDownProps {
+  visible: boolean;
+  handleClose: () => void;
+  handleOpen: () => void;
+  trigger: ReactNode;
+  children: ReactNode;
+  dropDownWidth?: number;
+}
+
+
+
+export const DropdownMenu: React.FC<DropDownProps> = ({
+  visible,
+  handleOpen,
+  handleClose,
+  trigger,
+  children,
+  dropDownWidth = 360,
+}) => {
+  const triggerRef = useRef<View>(null);
+  const [position, setPosition] = useState({x:0,y:0,width:0});
+
+  useEffect (()=>{
+    if(triggerRef.current && visible){
+      triggerRef.current.measure((fx, fy, width, height, px, py)=>{
+        setPosition({
+          x:px,
+          y:py+10,
+          width:width
+        });
+      });
+    };
+
+  },[visible])
+
+  return (
+    <View >
+      <TouchableWithoutFeedback onPress={handleOpen}>
+        <View ref={triggerRef}>{trigger}</View>
+      </TouchableWithoutFeedback>
+      {visible && (
+        <Modal transparent={true}
+               visible={true}
+               animationType='fade'
+               onRequestClose={handleClose} >
+                <TouchableWithoutFeedback onPress={handleClose}>
+                  <View className='flex-1 justify-start align-centert' >
+                    <View className='position-absolute bg-white rounded-md items-start px-1'
+                    style={{top:position.y, left:position.x + position.width / 2 - dropDownWidth / 2,
+                            width:dropDownWidth,
+                    }}>
+                      {children}
+                    </View>
+                  </View>
+
+                </TouchableWithoutFeedback>
+               </Modal>
+      )}
+    </View>
+  )
+
+}
+
--- Ext/app/otherPages/exercise_creation.tsx
+++ Ext/app/otherPages/exercise_creation.tsx
@@ -4,12 +4,15 @@ import CustomButton from '@/Components/button';
 import { createCustomExercise } from '../db/queries';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
+import { DropdownMenu, MenuOption } from '@/Components/dropDown';
 
 const Exercise_creation = () => {
 
   const router = useRouter();
 
   const [exerciseName, setExerciseName] = useState('');
+  const [muscleGroup, setMuscleGroup] = useState('None');
+  const [visible, setVisible] = useState(false);
 
   const pressButton = () => {
     try {
@@ -23,15 +26,35 @@ const Exercise_creation = () => {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView className='bg-dark-100' style={{flex: 1, alignItems: "center"}}> 
+      <SafeAreaView className='bg-dark-100 ' style={{flex: 1}}> 
 
-      <View className="mx-2 items-center mt-10">
-        <TextInput placeholder='Enter Exercise Name' 
+      <View className="mx-2 px-5 mt-10">
+        <Text className='items-start text-light-100 text-xl mb-3' >Exercise Name:</Text>
+        <TextInput 
                    value={exerciseName} 
                    onChangeText={setExerciseName} 
                    placeholderTextColor={'darkgrey'}
-                   className='text-center text-light-100'/>
-        {exerciseName.length>0 && <CustomButton onPress={()=>pressButton()} buttonText='Create Exercise' />}
+                   className='text-start text-white border-2 border-light-100 rounded-md h-[50]'/>
+        <Text className='items-start text-light-100 text-xl mt-5 mb-3' >Muscle Group:</Text>
+        <View>
+          <DropdownMenu visible={visible}
+                      handleOpen={()=>{setVisible(true)}}
+                      handleClose={()=>{setVisible(false)}}
+                      trigger={<Text className='text-lg py-2 w-[full] text-start px-2 bg-white rounded-lg text-light-100'>{muscleGroup}</Text>}>
+            <MenuOption onSelect={()=>{setVisible(false);setMuscleGroup('None')}}>
+              <View className=''>
+                <Text className='text-lg text-light-100'>None</Text>
+              </View>
+              
+              </MenuOption>
+            <MenuOption onSelect={()=>{setVisible(false);setMuscleGroup('Chest')}}><Text className='text-lg text-light-100'>Chest</Text></MenuOption>
+
+          </DropdownMenu>
+        </View>
+        <View className='mt-64'>
+          {exerciseName.length>0 && <CustomButton onPress={()=>pressButton()} buttonText='Create Exercise' />}
+        </View>
+        
       </View>
       
       
