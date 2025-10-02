d35cdad79815d530f1000c93f7ff12a99e28154b
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-10-02 11:12:02 +0200

    Trying to set up documentation before moving to instacning sessions

--- Ext/app/db/db.tsx
+++ Ext/app/db/db.tsx
@@ -1,86 +1,23 @@
 import * as SQLite from 'expo-sqlite';
+import { migrations } from './migrations';
 
 const db =  SQLite.openDatabaseSync('workouts.db');
 
+export const runMigrations = async() =>{
+    const [{user_version}] = db.getAllSync<{user_version:number}>("PRAGMA user_version;"); //Pragma is some sqlite command for changing features of database
+
+    for (const migration of migrations) {
+        if(migration.id > user_version){
+            db.execSync(migration.up);
+            db.execSync(`PRAGMA user_version=${migration.id}`);
+        }
+    }
+
+}
+
 
 export const initDB = async() => {
-
-    await db.execSync(
-        `CREATE TABLE IF NOT EXISTS workouts(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE
-        );`
-        
-    );
-    await db.execSync(
-        `CREATE TABLE IF NOT EXISTS muscle_groups(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT UNIQUE
-        );
-        
-        INSERT OR IGNORE INTO muscle_groups (name) VALUES
-        ('None'),
-        ('Chest'),
-        ('Back'),
-        ('Shoulders'),
-        ('Arms'),
-        ('Legs'),
-        ('Abs');`
-        
-    );
-    
-    await db.execSync(
-        `CREATE TABLE IF NOT EXISTS exercises_info(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE,
-        muscle_group INTEGER NOT NULL DEFAULT 1,
-        FOREIGN KEY (muscle_group) REFERENCES muscle_groups(id)
-        );`
-        
-    );
-    await db.execSync(
-        `CREATE TABLE IF NOT EXISTS exercises(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        workout_id INTEGER NOT NULL,
-        exercise_id INTEGER NOT NULL,
-        set_number INTEGER DEFAULT 1,
-        FOREIGN KEY (exercise_id) REFERENCES exercises_info(id),
-        FOREIGN KEY (workout_id) REFERENCES workouts(id)
-        );`
-    );
-    await db.execSync(
-        `CREATE TABLE IF NOT EXISTS sessions(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        workout_id INTEGER NOT NULL,
-        time_started INTEGER NOT NULL,
-        time_ended INTEGER,
-        FOREIGN KEY (workout_id) REFERENCES workouts(id)
-        );`
-        
-    );
-    await db.execSync(
-        `CREATE TABLE IF NOT EXISTS session_exercises(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        session_id INTEGER NOT NULL,
-        exercise_id INTEGER NOT NULL,
-        FOREIGN KEY (exercise_id) REFERENCES exercises(id),
-        FOREIGN KEY (session_id) REFERENCES sessions(id)
-        );`
-        
-    );
-    await db.execSync(
-        `CREATE TABLE IF NOT EXISTS session_sets(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        session_exercises_id INTEGER NOT NULL,
-        set_number INTEGER,
-        weight REAL,
-        reps INTEGER,
-        FOREIGN KEY (session_exercises_id) REFERENCES session_exercises(id)
-        );`
-        
-    );
-    
-    
+    await runMigrations();
 
 }
 
---
+++ Ext/app/db/migrations.tsx
@@ -0,0 +1,65 @@
+export const migrations = [
+    
+    {id:1,
+     up:`CREATE TABLE IF NOT EXISTS workouts(
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             name TEXT NOT NULL UNIQUE
+             );
+
+         CREATE TABLE IF NOT EXISTS muscle_groups(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE
+        );
+        
+        INSERT OR IGNORE INTO muscle_groups (name) VALUES
+        ('None'),
+        ('Chest'),
+        ('Back'),
+        ('Shoulders'),
+        ('Arms'),
+        ('Legs'),
+        ('Abs');
+
+
+        CREATE TABLE IF NOT EXISTS exercises_info(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE,
+        muscle_group INTEGER NOT NULL DEFAULT 1,
+        FOREIGN KEY (muscle_group) REFERENCES muscle_groups(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS exercises(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        workout_id INTEGER NOT NULL,
+        exercise_id INTEGER NOT NULL,
+        set_number INTEGER DEFAULT 1,
+        FOREIGN KEY (exercise_id) REFERENCES exercises_info(id),
+        FOREIGN KEY (workout_id) REFERENCES workouts(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS sessions(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        workout_id INTEGER NOT NULL,
+        time_started INTEGER NOT NULL,
+        time_ended INTEGER,
+        FOREIGN KEY (workout_id) REFERENCES workouts(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS session_exercises(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        session_id INTEGER NOT NULL,
+        exercise_id INTEGER NOT NULL,
+        FOREIGN KEY (exercise_id) REFERENCES exercises(id),
+        FOREIGN KEY (session_id) REFERENCES sessions(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS session_sets(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        session_exercises_id INTEGER NOT NULL,
+        set_number INTEGER,
+        weight REAL,
+        reps INTEGER,
+        FOREIGN KEY (session_exercises_id) REFERENCES session_exercises(id)
+        );`,
+    },
+]
