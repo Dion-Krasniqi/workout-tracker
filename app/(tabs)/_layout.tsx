b11f921d9496e653d673862e4e1ed7405fb5e95a
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-11-30 16:34:38 +0100

    Tab bar icons added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,22 @@
 
+import { icons } from "@/constants/icons";
 import { Tabs } from "expo-router";
-import { Text } from 'react-native';
+import { Dimensions, Image, Text } from 'react-native';
 
 const Ticon = ({focused, title}:any) => {
-  // add icons
-  if (focused) {
-    return (
-      <Text className='flex-1 font-bold w-full flex-1 min-w-[120px]
-                    min-h-14 mt-16 text-center overflow-hidden'>{title}</Text>
-
-    )}
+  // icons are a bit different in size and how they fit so have to treat each diff
+  const Width = Dimensions.get("window").width;
+  if (focused ) {
+    if (title == 'Home'){
+      return (
+        <Image  style={{width:Width/12, height:Width/12, marginTop:25}} source={icons.homepage} />)
+    } else {
+      return (
+        <Image  style={{width:Width/6, height:Width/6, marginTop:25}} source={icons.workouts} />)
+    }
+    }
   return (
-      <Text className='flex-1  w-full flex-1 min-w-[120px]
+      <Text className='flex-1  w-full min-w-[120px]
                     min-h-14 mt-16 text-center overflow-hidden text-light-100'>{title}</Text>
 
     )
@@ -25,14 +30,14 @@ const TabLayout = () => {
                                            justifyContent:'center',
                                            alignItems:'center',},
                           tabBarStyle:{borderRadius:25,
-                                       marginBottom:50,
+                                       marginBottom:40,
                                        marginHorizontal:10,
                                        height:70,
                                        position:'absolute',}}}>
       <Tabs.Screen name="index" options={{ title:'Home', headerShown:false, tabBarIcon:({ focused })=> (
-                <Ticon focused={focused} title='Home'/>)}}/>
+                <Ticon focused={focused} title='Home' />)}}/>
       <Tabs.Screen name="workouts" options={{ title:'Workouts', headerShown:false, tabBarIcon:({ focused })=> (
-                <Ticon focused={focused} title='Workouts'/>)}}/>
+                <Ticon focused={focused} title='Workouts'  />)}}/>
     </Tabs>
   )
 }
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,5 +1,9 @@
 import hamburger from "@/assets/icons/hamburger.png";
+import homepage from "@/assets/icons/homepage.png";
+import workouts from "@/assets/icons/workouts.png";
 
 export const icons = {
   hamburger,
+  homepage,
+  workouts
 };
